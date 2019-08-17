ecc0a8cad058f6ed48545ec2a0b8c14445b9fa7d
geekerstar <247507792@qq.com>
2019-08-17 09:46:20 +0800

    跨域配置

---
+++ springboot-helloworld/src/main/java/com/geekerstar/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package com.geekerstar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author geekerstar
+ * date: 2019-08-17 09:41
+ * description:
+ *
+ * 跨域配置
+ */
+@Configuration
+public class CORSConfig extends WebMvcConfigurerAdapter {
+
+    public void addCorsMappings(CorsRegistry registry){
+        // 允许所有跨域访问
+//        registry.addMapping("/**");
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://wwww.geekerstar.com")
+                .allowedMethods("POST","GET");
+    }
+}
