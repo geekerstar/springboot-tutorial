6e4cfe7684e083e1a757ebe3f52f776e82f6ec75
geekerstar <247507792@qq.com>
2019-08-17 09:58:17 +0800

    配置Jackson实现日期格式化

--- springboot-helloworld/src/main/java/com/geekerstar/controller/TimeController.java
+++ springboot-helloworld/src/main/java/com/geekerstar/controller/TimeController.java
@@ -2,6 +2,8 @@ package com.geekerstar.controller;
 
 import com.geekerstar.entity.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,8 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimeController {
 
     @PostMapping("/time")
-    public String createTime(User user){
+    public String createTime(@RequestBody User user){
         System.out.println(user.getCreateTime().toString());
         return "success";
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user){
+        System.out.println(user.getCreateTime().toString());
+        // 将User以JSON格式返回
+        return user;
     }
 }
