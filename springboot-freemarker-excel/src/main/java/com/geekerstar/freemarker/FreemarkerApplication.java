2e007e983005e40e20ad8ff29508903c97d5ec55
geekerstar <247507792@qq.com>
2019-08-19 09:54:45 +0800

    SpringBoot使用freemarker生成Excel

---
+++ springboot-freemarker-excel/src/main/java/com/geekerstar/freemarker/FreemarkerApplication.java
@@ -0,0 +1,16 @@
+package com.geekerstar.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author geekerstar
+ * date: 2019-08-19 09:43
+ * description:
+ */
+@SpringBootApplication
+public class FreemarkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerApplication.class,args);
+    }
+}
---
+++ springboot-freemarker-excel/src/test/java/com/geekerstar/freemarker/entity/Commodity.java
@@ -0,0 +1,21 @@
+package com.geekerstar.freemarker.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@Builder
+public class Commodity {
+    private String     name;
+    private Integer    num;
+    private int        num1;
+    private BigDecimal price;
+    private Double     price2;
+    private Float price3;
+    private Date       createTime;
+    private Long       timestamp;
+}
