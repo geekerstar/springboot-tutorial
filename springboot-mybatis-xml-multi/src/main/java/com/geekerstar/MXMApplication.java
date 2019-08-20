4c5164c07b993afd5fbfe65190519637ef5577cd
geekerstar <247507792@qq.com>
2019-08-20 17:17:45 +0800

    SpringBoot整合mybatis多数据源xml形式

---
+++ springboot-mybatis-xml-multi/src/main/java/com/geekerstar/MXMApplication.java
@@ -0,0 +1,12 @@
+package com.geekerstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MXMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MXMApplication.class, args);
+	}
+}
---
+++ springboot-mybatis-xml-multi/src/main/java/com/geekerstar/mapper/test1/User1Mapper.java
@@ -0,0 +1,19 @@
+package com.geekerstar.mapper.test1;
+
+import com.geekerstar.model.User;
+
+import java.util.List;
+
+public interface User1Mapper {
+
+	List<User> getAll();
+
+	User getOne(Long id);
+
+	void insert(User user);
+
+	void update(User user);
+
+	void delete(Long id);
+
+}
