1f66a5bdafaee8f7d4c82f28485271c4512b8f64
geekerstar <247507792@qq.com>
2019-09-03 21:43:12 +0800

    SpringBoot源码研究2

---
+++ springboot-source/overview/src/main/java/com/geekerstar/diveinspringboot/DiveInSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.geekerstar.diveinspringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+//@ServletComponentScan(basePackages = "com.imooc.diveinspringboot.web.servlet")
+public class DiveInSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(DiveInSpringBootApplication.class)
+//				.web(WebApplicationType.NONE)
+                .run(args);
+//		SpringApplication.run(DiveInSpringBootApplication.class, args);
+    }
+}
