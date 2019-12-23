e725bb1e8e8459ff0aa4072b670f68dd3c020379
geekerstar <247507792@qq.com>
2019-12-23 22:11:22 +0800

    SpringBoot读取配置

---
+++ springboot-resource/src/main/java/com/geekerstar/resource/ResourceApplication.java
@@ -0,0 +1,16 @@
+package com.geekerstar.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author geekerstar
+ * date: 2019/12/22 18:34
+ * description:
+ */
+@SpringBootApplication
+public class ResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class,args);
+    }
+}
---
+++ springboot-resource/src/main/java/com/geekerstar/resource/pojo/Resource.java
@@ -0,0 +1,43 @@
+package com.geekerstar.resource.pojo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author geekerstar
+ * date: 2019/12/22 18:37
+ * description:
+ */
+@Configuration
+@ConfigurationProperties(prefix = "com.boot")
+@PropertySource(value = "classpath:resource.properties")
+public class Resource {
+    private String name;
+    private String website;
+    private String language;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
