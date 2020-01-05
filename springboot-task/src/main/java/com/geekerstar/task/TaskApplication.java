3065038877307b352e1910dc1182de41185e39f5
geekerstar <247507792@qq.com>
2020-01-05 22:56:37 +0800

    task

---
+++ springboot-task/src/main/java/com/geekerstar/task/TaskApplication.java
@@ -0,0 +1,16 @@
+package com.geekerstar.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author geekerstar
+ * date: 2019/12/29 16:39
+ * description:
+ */
+@SpringBootApplication
+public class TaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class,args);
+    }
+}
---
+++ springboot-task/src/main/java/com/geekerstar/task/tasks/TestTask.java
@@ -0,0 +1,20 @@
+package com.geekerstar.task.tasks;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class TestTask {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+//     @Scheduled(fixedRate = 3000)
+    @Scheduled(cron = "4-40 * * * * ?")
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+
+}
