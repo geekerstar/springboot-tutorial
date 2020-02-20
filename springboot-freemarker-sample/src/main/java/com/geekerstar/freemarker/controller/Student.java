c300ad939e85bf8f6c164e201e779a16465e4b0b
geekerstar <247507792@qq.com>
2020-02-20 14:11:05 +0800

    freemarker 遍历list对象测试

---
+++ springboot-freemarker-sample/src/main/java/com/geekerstar/freemarker/controller/Student.java
@@ -0,0 +1,16 @@
+package com.geekerstar.freemarker.controller;
+
+import lombok.Data;
+
+/**
+ * @author geekerstar
+ * @date 2020/2/20 14:03
+ * @description
+ */
+@Data
+public class Student {
+    private String name;
+
+    private String sex;
+
+}
