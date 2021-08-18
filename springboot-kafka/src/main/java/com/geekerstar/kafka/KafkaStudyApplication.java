c912cb2364e8058fb6b6084ebf9733ad84b49bbf
geekerstar <247507792@qq.com>
2021-08-18 16:31:34 +0800

    kafka

---
+++ springboot-kafka/src/main/java/com/geekerstar/kafka/KafkaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.geekerstar.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStudyApplication.class, args);
+    }
+
+}
